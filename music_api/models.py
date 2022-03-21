3aafa52e70c60fee3d8ae92d8b36e1d91fac2f56
Gover Braam <groverbraam@gmail.com>
2022-03-21 14:27:11 -0400

    added audio uploead to the backend

--- music_api/models.py
+++ music_api/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-
 # Create your models here.
 
+
 class Song(models.Model):
-    name = models.CharField(max_length = 100, blank = True)
-    artist = models.CharField(max_length = 100, blank = True)
+    name = models.CharField(max_length=100, blank=True)
+    artist = models.CharField(max_length=100, blank=True)
     GENRE_CHOICES = [
         ('pop', 'Pop'),
         ('rock', 'Rock'),
@@ -18,12 +18,10 @@ class Song(models.Model):
         ('alternative', 'Alternative'),
         ('indie', 'Indie'),
     ]
-    genre = models.CharField(max_length = 100, choices=GENRE_CHOICES, default='', blank = True)
-    audio = models.CharField(max_length = 100, blank = True)
-    image = models.CharField(max_length = 100, default = 'https://i.imgur.com/D3aOVsJ.png', blank = True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, default = '1.29', blank = True)
-
-    
-    
-
-    
+    genre = models.CharField(
+        max_length=100, choices=GENRE_CHOICES, default='Pop', blank=True)
+    audio = models.FileField(upload_to='uploads/')
+    image = models.CharField(
+        max_length=100, default='https://i.imgur.com/D3aOVsJ.png', blank=True)
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2, default='1.29', blank=True)

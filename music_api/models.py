4249a535472b078a70448b0c0a95966cee496dc9
Gover Braam <groverbraam@gmail.com>
2022-03-21 15:53:16 -0400

    change the audio model path

--- music_api/models.py
+++ music_api/models.py
@@ -18,17 +18,10 @@ class Song(models.Model):
         ('alternative', 'Alternative'),
         ('indie', 'Indie'),
     ]
-<<<<<<< HEAD
     genre = models.CharField(
-        max_length=100, choices=GENRE_CHOICES, default='Pop', blank=True)
-    audio = models.FileField(upload_to='uploads/')
+        max_length=100, choices=GENRE_CHOICES, default='', blank=True)
+    audio = models.FileField(upload_to='api/songs/')
     image = models.CharField(
         max_length=100, default='https://i.imgur.com/D3aOVsJ.png', blank=True)
     price = models.DecimalField(
         max_digits=6, decimal_places=2, default='1.29', blank=True)
-=======
-    genre = models.CharField(max_length = 100, choices=GENRE_CHOICES, default='', blank = True)
-    audio = models.FileField(upload_to='uploads/')
-    image = models.CharField(max_length = 100, default = 'https://i.imgur.com/D3aOVsJ.png', blank = True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, default = '1.29', blank = True)
->>>>>>> ade889f3814389121a1cb8de55d9036d73d66240
